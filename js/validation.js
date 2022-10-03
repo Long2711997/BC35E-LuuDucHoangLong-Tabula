460843a040ecae6686286726a249dcb07ad70096
Your Name <you@example.com>
2022-10-03 19:54:05 +0700

    fix js

--- js/validation.js
+++ js/validation.js
@@ -1,36 +1,46 @@
+function checkNull(name, email){
+    
+    if (name === ""){
+        document.getElementById('errorName').innerHTML = "(*) Name must not be blank";
+    }
+
+    if ( email === ""){
+        document.getElementById('errorMail').innerHTML = "(*) Email must not be blank";
+    }
+
+    return false;
+}
+
+function checkMail(email){
+    var letter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+        if ( email.match(letter)){
+            return true;
+        } else {
+            document.getElementById('errorMail').innerHTML = "(*) Input Email is invalid";
+            return false;
+    }
+}
+
 var submit = document.getElementById('submit');
 submit.onclick = function () {
     var name = document.getElementById('inputName').value;
     var email = document.getElementById('inputEmail').value;
 
-    
-        kiemTraRong = function(value, divError, mess){
-        if (value.trim() === ""){
-        //Thong bao loi
-        getEle(divError).innerHTML = mess;
-        getEle(divError).style.display = "block";
+    var check = checkNull(name, email);
+    var checkE = checkMail(email);
+
+    do {
         return false;
-    } 
-        getEle(divError).innerHTML = '';
-        getEle(divError).style.display = "none";
-        return true;
-    }
+} while (!check || !checkE);
+
+    // if (!check){
+    //     return false;
+    // }
+
+    // if (!checkE){
+    //     return false;
+    // }
     
-        kiemTraMail = function(value, divError, mess){
-            var letter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-            if (value.match(letter)) {
-                getEle(divError).innerHTML = "";
-                getEle(divError).style.display = "none";
-                return true;
-            }
-                getEle(divError).innerHTML = mess;
-                getEle(divError).style.display = "block";
-                return false;
-        }
-
-    var isValid = true;
-
-    isValid &= validation.kiemTraRong(name, "errorName", "(*) Name must not be blank") && validation.kiemTraRong(email, "errorMail", "(*) Email must not be blank");
-    isValid &= validation.kiemTraMail(email, "errorMail", "(*) Input Email is invalid");
 
 }
